37ffdbaab2cb346b0530d5215b642fdd285da22f
PavanMeka09 <Mekapavan99@gmail.com>
2024-03-05 22:55:43 +0530

    Fixes

--- fetch.js
+++ fetch.js
@@ -81,7 +81,7 @@ if (USE_GITHUB_DATA === "true") {
     res.on("end", () => {
       fs.writeFile("./public/profile.json", data, function (err) {
         if (err) return console.log(err);
-        console.log("saved file to public/profile.json");
+        console.log("saved file to /profile.json");
       });
     });
   });
@@ -117,7 +117,7 @@ if (MEDIUM_USERNAME !== undefined) {
     res.on("end", () => {
       fs.writeFile("./public/blogs.json", mediumData, function (err) {
         if (err) return console.log(err);
-        console.log("saved file to public/blogs.json");
+        console.log("saved file to blogs.json");
       });
     });
   });
